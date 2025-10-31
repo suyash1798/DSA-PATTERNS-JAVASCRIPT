f82ded42623731e343044d5dee141964387037fc
Suyash Tiwari <suyashtiwari1798@gmail.com>
2025-10-31 17:26:14 +0000

    Count Nodes Equal to Average of Subtree

---
+++ Tree/Count-Nodes-Equal-to-Average-of-Subtree.js
@@ -0,0 +1,64 @@
+/*
+
+Given the root of a binary tree, return the number of nodes where the value of the node is equal to the average of the values in its subtree.
+
+Note:
+
+The average of n elements is the sum of the n elements divided by n and rounded down to the nearest integer.
+A subtree of root is a tree consisting of root and all of its descendants.
+
+
+Example 1:
+
+
+Input: root = [4,8,5,0,1,null,6]
+Output: 5
+Explanation: 
+For the node with value 4: The average of its subtree is (4 + 8 + 5 + 0 + 1 + 6) / 6 = 24 / 6 = 4.
+For the node with value 5: The average of its subtree is (5 + 6) / 2 = 11 / 2 = 5.
+For the node with value 0: The average of its subtree is 0 / 1 = 0.
+For the node with value 1: The average of its subtree is 1 / 1 = 1.
+For the node with value 6: The average of its subtree is 6 / 1 = 6.
+
+*/
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var averageOfSubtree = function(root) {
+    let count = 0;
+
+    function traverse(root){
+        if(!root){
+            return { sum: 0, nodes: 0};
+        }
+        
+        let left = traverse(root.left);
+        let right = traverse(root.right);
+
+        let sum = root.val + left.sum + right.sum;
+        let nodes = 1 + left.nodes + right.nodes;
+
+        let avg = Math.floor(sum / nodes);
+
+        if(avg === root.val){
+            count++;
+        }
+
+        return {sum: sum, nodes: nodes};
+    }
+
+    traverse(root);
+
+    return count;
+};
