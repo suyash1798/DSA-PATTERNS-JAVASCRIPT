d227ff72b6749e68a29ab358dc5e34562ba7e1b5
Suyash Tiwari <suyashtiwari1798@gmail.com>
2025-11-19 18:15:26 +0000

    Maximum Alternating Sum of Squares

---
+++ Greedy/Maximum-Alternating-Sum-of-Squares.js
@@ -0,0 +1,72 @@
+/*
+
+You are given an integer array nums. You may rearrange the elements in any order.
+
+The alternating score of an array arr is defined as:
+
+score = arr[0]2 - arr[1]2 + arr[2]2 - arr[3]2 + ...
+Return an integer denoting the maximum possible alternating score of nums after rearranging its elements.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3]
+
+Output: 12
+
+Explanation:
+
+A possible rearrangement for nums is [2,1,3], which gives the maximum alternating score among all possible rearrangements.
+
+The alternating score is calculated as:
+
+score = 22 - 12 + 32 = 4 - 1 + 9 = 12
+
+Example 2:
+
+Input: nums = [1,-1,2,-2,3,-3]
+
+Output: 16
+
+Explanation:
+
+A possible rearrangement for nums is [-3,-1,-2,1,3,2], which gives the maximum alternating score among all possible rearrangements.
+
+The alternating score is calculated as:
+
+score = (-3)2 - (-1)2 + (-2)2 - (1)2 + (3)2 - (2)2 = 9 - 1 + 4 - 1 + 9 - 4 = 16
+
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+-4 * 104 <= nums[i] <= 4 * 104
+
+
+*/
+
+
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxAlternatingSum = function (nums) {
+    nums.sort((a, b) => Math.pow(b, 2) - Math.pow(a, 2));
+
+    let sum = 0;
+
+    let start = 0, end = nums.length - 1;
+
+    while (start <= end) {
+        sum += Math.pow(nums[start], 2);
+
+        if (start !== end) sum -= Math.pow(nums[end], 2);
+
+        start++; end--;
+    }
+
+    return sum;
+};
