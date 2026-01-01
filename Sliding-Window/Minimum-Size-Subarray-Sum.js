8cec895266f4c8294c784895428c814e8c9e50e2
Suyash Tiwari <suyashtiwari1798@gmail.com>
2026-01-01 20:26:30 +0000

    Minimum Size Subarray Sum

---
+++ Sliding-Window/Minimum-Size-Subarray-Sum.js
@@ -0,0 +1,53 @@
+/*
+
+Given an array of positive integers nums and a positive integer target, return the minimal length of a subarray whose sum is greater than or equal to target. If there is no such subarray, return 0 instead.
+
+ 
+
+Example 1:
+
+Input: target = 7, nums = [2,3,1,2,4,3]
+Output: 2
+Explanation: The subarray [4,3] has the minimal length under the problem constraint.
+Example 2:
+
+Input: target = 4, nums = [1,4,4]
+Output: 1
+Example 3:
+
+Input: target = 11, nums = [1,1,1,1,1,1,1,1]
+Output: 0
+ 
+
+Constraints:
+
+1 <= target <= 109
+1 <= nums.length <= 105
+1 <= nums[i] <= 104
+
+
+*/
+
+
+
+/**
+ * @param {number} target
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minSubArrayLen = function (target, nums) {
+    let start = 0, min = Infinity, sum = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        sum += nums[i];
+
+        while ((sum - nums[start]) >= target) {
+            sum -= nums[start];
+            start++;
+        }
+
+        if (sum >= target) min = Math.min(min, i - start + 1);
+    }
+
+    return min === Infinity ? 0 : min;
+};
